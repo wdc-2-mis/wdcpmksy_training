f9bcce2075bdc1896d57c92f0207d62946437e61
hp <hp@DESKTOP-MVP1JQH>
2024-12-06 13:09:08 +0530

    COMMIT BY HIMANSHU

--- src/main/java/com/springboot/training/controller/AddQuestionController.java
+++ src/main/java/com/springboot/training/controller/AddQuestionController.java
@@ -1,18 +1,27 @@
 package com.springboot.training.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.springboot.training.dto.AddCourseQuestion;
 import com.springboot.training.dto.CourseDetails;
 import com.springboot.training.entity.LMSTrainingDetails;
 import com.springboot.training.repository.CourseDtlRepository;
 import com.springboot.training.repository.LmsTrainingQuestionRepository;
+import com.springboot.training.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 @Controller
@@ -24,12 +33,19 @@ public class AddQuestionController {
     @Autowired
     private LmsTrainingQuestionRepository trainingQuestionRepository; 
 
+    @Autowired
+    private UserService userService;
+    
     @GetMapping("/addCourseQuestion")
     public String addCourseQuestion(HttpSession session, Model model) {
         String userId = (String) session.getAttribute("userId");
-        List<LMSTrainingDetails> courses = courseDtlRepository.findAll();
+        AddCourseQuestion question = new AddCourseQuestion();
+        //List<LMSTrainingDetails> courses = courseDtlRepository.findAll();
+        
+        List<LMSTrainingDetails> courses = courseDtlRepository.findCoursesdtl();
 
         model.addAttribute("courses", courses);
+        model.addAttribute("question", question);
         model.addAttribute("userId", userId);
 
         return "addCourseques";
@@ -56,4 +72,41 @@ public class AddQuestionController {
 
         return questionCount + 1;
     }
+
+	
+    @PostMapping("/saveCourseQuestion")
+    public String saveCourseQuestion(
+        @Valid @ModelAttribute("question") AddCourseQuestion question,
+        BindingResult result, Model model, HttpSession session, @RequestParam(required = false) String action, RedirectAttributes redirectAttributes) {
+    	List<LMSTrainingDetails> courses = courseDtlRepository.findCoursesdtl();
+        
+    	if (result.hasErrors()) {
+            Integer checkcourse = question.getCourseId();
+            model.addAttribute("courses", courses);
+            model.addAttribute("userId", session.getAttribute("userId"));
+            model.addAttribute("checkcourse", checkcourse);
+
+            return "addCourseques"; 
+        }
+
+        userService.saveaddQuestion(question, session);
+
+        if ("submit".equals(action)) {
+        	 userService.updateStatusToComplete(question.getCourseId());
+        	 redirectAttributes.addFlashAttribute("successMessage", "Question saved successfully!");
+             return "redirect:/addCourseQuestion"; 
+        } else {
+        	redirectAttributes.addFlashAttribute("successMessage", "Question saved successfully. Please add the next question.");
+            Integer checkcourse = question.getCourseId();
+            model.addAttribute("courses", courses);
+            model.addAttribute("userId", session.getAttribute("userId"));
+            model.addAttribute("checkcourse", checkcourse);
+            return "redirect:/addCourseQuestion";
+        }
+    }
+
+    
+
+	 
+
 }
--- src/main/java/com/springboot/training/service/UserService.java
+++ src/main/java/com/springboot/training/service/UserService.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import com.springboot.training.dto.AddCourseQuestion;
 import com.springboot.training.dto.CourseDetails;
 import com.springboot.training.dto.UserDto;
 import com.springboot.training.dto.UserUrlDto;
 import com.springboot.training.entity.URLDetails;
 import com.springboot.training.entity.User;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 
@@ -26,4 +28,7 @@ public interface UserService {
 	Page<URLDetails> findPaginated(int PageNo, int PageSize);
 
 	void saveCourseDetailUrl(CourseDetails courseDetails, String ext, String file_name, String filePath, String userId, Integer regId);
+   
+	void saveaddQuestion(@Valid AddCourseQuestion question, HttpSession session);
+    void updateStatusToComplete(Integer courseId);
 }
