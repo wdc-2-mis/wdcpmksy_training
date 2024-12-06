df463d97f22ba03fe23d16f167eecb0f6638645d
hp <hp@DESKTOP-FLSV6G7>
2024-12-06 10:39:48 +0530

    view course detail controller commit

--- src/main/java/com/springboot/training/controller/ViewCourseAndAddQuestionController.java
+++ src/main/java/com/springboot/training/controller/ViewCourseAndAddQuestionController.java
@@ -2,11 +2,14 @@ package com.springboot.training.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springboot.training.dto.CourseDetails;
 import com.springboot.training.entity.LMSTrainingDetails;
@@ -36,6 +39,25 @@ public class ViewCourseAndAddQuestionController {
 //		}
 		
 		return "viewCourseDetails";
+		
+	}
+	
+	@GetMapping("/viewCourseDetail/{id}")
+	public String viewCourseDtl(@PathVariable("id") int id, Model model) {
+		List<LMSTrainingDetails> lmsTrainingList =  new ArrayList<>();
+		LMSTrainingDetails list = courseDtlRepo.findById(id).orElse(null);
+		lmsTrainingList.add(list);
+		model.addAttribute("lmsTrainingList", lmsTrainingList);
+		model.addAttribute("lmsTrainingListSize", lmsTrainingList.size());
+//		List<CourseDetails> courseDtlList =  new ArrayList<>();
+//		for(LMSTrainingDetails dtl: lmsTrainingList) {
+//			CourseDetails courseDtl = new CourseDetails();
+//			courseDtl.setCname(dtl.getCourse_name());
+//			courseDtl.setCdesc(dtl.getCourse_description());
+//			courseDtl.setTotquest(dtl.getNoof_question());
+//		}
+		
+		return "getCourseDetails";
 		
 	}
 }
