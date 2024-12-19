092e34c370026906f0bd343c0b834d16351f1b42
NIC <NIC@DESKTOP-IGQMEEK>
2024-12-19 10:22:13 +0530

    kdy

--- src/main/java/com/springboot/training/entity/LmsUserQuizDetails.java
+++ src/main/java/com/springboot/training/entity/LmsUserQuizDetails.java
@@ -63,6 +63,113 @@ public class LmsUserQuizDetails {
 	@ManyToOne
 	@JoinColumn(name = "user_reg_id", referencedColumnName = "user_reg_id", insertable = false, updatable = false)
 	private User user;
+
+	
+	
+	
+	public Integer getUser_quiz_id() {
+		return user_quiz_id;
+	}
+
+	public void setUser_quiz_id(Integer user_quiz_id) {
+		this.user_quiz_id = user_quiz_id;
+	}
+
+	public Integer getTrainingId() {
+		return trainingId;
+	}
+
+	public void setTrainingId(Integer trainingId) {
+		this.trainingId = trainingId;
+	}
+
+	public Integer getUserRegId() {
+		return userRegId;
+	}
+
+	public void setUserRegId(Integer userRegId) {
+		this.userRegId = userRegId;
+	}
+
+	public Integer getMarksIbtained() {
+		return marksIbtained;
+	}
+
+	public void setMarksIbtained(Integer marksIbtained) {
+		this.marksIbtained = marksIbtained;
+	}
+
+	public Integer getQuestionAttempt() {
+		return questionAttempt;
+	}
+
+	public void setQuestionAttempt(Integer questionAttempt) {
+		this.questionAttempt = questionAttempt;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRequestedIp() {
+		return requestedIp;
+	}
+
+	public void setRequestedIp(String requestedIp) {
+		this.requestedIp = requestedIp;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LMSTrainingDetails getTrainingDetails() {
+		return trainingDetails;
+	}
+
+	public void setTrainingDetails(LMSTrainingDetails trainingDetails) {
+		this.trainingDetails = trainingDetails;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 	
 	
 	
